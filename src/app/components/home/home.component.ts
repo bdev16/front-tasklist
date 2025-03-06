d0696bcfe2e8384418f406e9e6e77419c1844454
DevBruno <brunoantonio.dev0016@gmail.com>
2025-03-06 15:43:49 -0300

    feat: Criando e utilizando funções para dispositivos moveis

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  visibleOptions: boolean = false;
+  visibleLinksNavbarMobile: boolean = false;
+
+  viewMobileOptions() {
+    this.visibleOptions = !this.visibleOptions;
+  }
+
+  viewNavbarMobileOptions() {
+    this.visibleLinksNavbarMobile = !this.visibleLinksNavbarMobile;
+  }
 
 }
