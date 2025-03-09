2b0df29a69ca287194557c29457de2b6ebb65a79
DevBruno <brunoantonio.dev0016@gmail.com>
2025-03-08 21:23:49 -0300

    feat(home.component.ts): Cria objeto do tipo Date que vai compor a data atual e vai ser utilizado dentro do input do matdatepicker

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
-import { FlatpickrDirective, provideFlatpickrDefaults } from 'angularx-flatpickr';
 import { RouterModule } from '@angular/router';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule, FlatpickrDirective, RouterModule],
+  imports: [CommonModule, RouterModule, MatDatepickerModule, MatInputModule, MatFormFieldModule, FormsModule, MatNativeDateModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
+  currentDate: Date = new Date();
   visibleOptions: boolean = false;
   visibleLinksNavbarMobile: boolean = false;
 
